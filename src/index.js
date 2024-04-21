81281d07300ff5b3cb3637d300db3a1fe35c0236
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2024-04-20 22:04:10 -0600

    Actualización de dependencias de React a la versión 17.0.2 en package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/';
+import {render} from 'react-dom'
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createPortal(document.getElementById('root'));
+// root.render(<App />);
+
+const root = document.getElementById('root');
+render(<App />, root);
